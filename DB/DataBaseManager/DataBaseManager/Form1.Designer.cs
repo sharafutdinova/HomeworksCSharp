bc50a2f19db3d1aa810158e1e33af009416a1e0f
Alsou <salsou96@mail.ru>
2016-04-19 21:41:31 +0300

    Added files via upload

---
+++ DB/DataBaseManager/DataBaseManager/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace DataBaseManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OkButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.deleteRow = new System.Windows.Forms.Button();
+            this.saveAllChanges = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(523, 9);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(75, 23);
+            this.OkButton.TabIndex = 1;
+            this.OkButton.Text = "OK";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(411, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Data base name:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(15, 53);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(713, 349);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // deleteRow
+            // 
+            this.deleteRow.Location = new System.Drawing.Point(15, 423);
+            this.deleteRow.Name = "deleteRow";
+            this.deleteRow.Size = new System.Drawing.Size(159, 23);
+            this.deleteRow.TabIndex = 5;
+            this.deleteRow.Text = "Delete Row";
+            this.deleteRow.UseVisualStyleBackColor = true;
+            this.deleteRow.Click += new System.EventHandler(this.deleteRow_Click);
+            // 
+            // saveAllChanges
+            // 
+            this.saveAllChanges.Location = new System.Drawing.Point(194, 423);
+            this.saveAllChanges.Name = "saveAllChanges";
+            this.saveAllChanges.Size = new System.Drawing.Size(159, 23);
+            this.saveAllChanges.TabIndex = 6;
+            this.saveAllChanges.Text = "Save all changes";
+            this.saveAllChanges.UseVisualStyleBackColor = true;
+            this.saveAllChanges.Click += new System.EventHandler(this.saveAllChanges_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 458);
+            this.Controls.Add(this.saveAllChanges);
+            this.Controls.Add(this.deleteRow);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OkButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button deleteRow;
+        private System.Windows.Forms.Button saveAllChanges;
+    }
+}
+
---
+++ DB/DataBaseManager/DataBaseManager/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Windows.Forms;
+using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace DataBaseManager
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog folderBrowserDialog = null;
+        DialogResult dialogResult;
+        DataSet dataBase;
+        TextReader reader;
+        TabPage tabPage;
+        DataGridView dgv;
+        List <DataGridView> tables;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tables = new List<DataGridView>();
+        }
+
+        // выбор баззы данных
+        public void SelectTheDatabase()
+        {
+            this.folderBrowserDialog = new FolderBrowserDialog();
+            this.dialogResult = this.folderBrowserDialog.ShowDialog();
+            this.textBox1.Text = this.folderBrowserDialog.SelectedPath;
+        }
+
+        // считывание таблиц
+        public void ReadingTables()
+        {
+            this.reader = new StreamReader(this.folderBrowserDialog.SelectedPath + @"\main.txt");
+
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                this.dataBase.Tables.Add(line);
+                line = reader.ReadLine();
+            }
+        }
+
+        public void AddingColumns(DataTable table)
+        {
+            this.reader = new StreamReader(this.folderBrowserDialog.SelectedPath + @"\" + table.TableName + @"\main.txt");
+            string column = reader.ReadLine();
+            while (column != null)
+            {
+                table.Columns.Add(column);
+                column = reader.ReadLine();
+            }
+            reader.Close();
+        }
+
+        public void AddingData(DataTable table)
+        {
+            this.reader = new StreamReader(this.folderBrowserDialog.SelectedPath + @"\" + table.TableName + @"\data.txt");
+            string data = reader.ReadLine();
+            while (data != null)
+            {
+                table.Rows.Add(data.Split(' '));
+                data = reader.ReadLine();
+            }
+            reader.Close();
+        }
+
+        // считывание схем таблиц
+        public void ReadingTablesSchema()
+        {
+            foreach (DataTable table in this.dataBase.Tables)
+            {
+                AddingColumns(table);
+
+                AddingData(table);
+            }
+        }
+
+        public DataGridView CreateDataGridView()
+        {
+            dgv = new DataGridView();
+            dgv.Width = 700;
+            dgv.Height = 300;
+            return dgv;
+        }
+
+        public void DisplayData()
+        {
+            foreach (DataTable table in this.dataBase.Tables)
+            {
+                tabPage = new TabPage(table.TableName);
+                tabControl1.TabPages.Add(tabPage);
+                dgv = CreateDataGridView();
+                dgv.DataSource = table;
+                tabPage.Controls.Add(dgv);
+                tables.Add(dgv);
+            }      
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            this.dataBase = new DataSet();
+
+            SelectTheDatabase();
+
+            ReadingTables();
+
+            ReadingTablesSchema();
+
+            DisplayData();
+        }
+
+        private void deleteRow_Click(object sender, EventArgs e)
+        {
+            tabPage = tabControl1.SelectedTab;
+            bool find = false;
+            int i = 0;
+            while (!find && i < tabControl1.TabCount)
+            {
+                if (tabControl1.TabPages[i] == tabPage)
+                {
+                    int b = 0;
+                    int j = tables[i].SelectedRows[b].Index;
+                    //tables[i].Rows.RemoveAt(j);
+                    dataBase.Tables[i].Rows.RemoveAt(j);
+                    //break;
+                    find = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+
+        private void saveAllChanges_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw;
+            foreach (DataTable table in dataBase.Tables)
+            {
+                sw = new StreamWriter(this.folderBrowserDialog.SelectedPath + @"\" + table.TableName + @"\data.txt");
+                int countCol = table.Columns.Count;
+                int countR = table.Rows.Count;
+                for (int i = 0; i < countR; i++)
+                {
+                    string line = "";
+                    for (int j = 0; j < countCol - 1; j++)
+                    {
+                        line += table.Rows[i][j].ToString() + " ";
+                    }
+                    line += table.Rows[i][countCol - 1].ToString();
+                    sw.WriteLine(line);
+                }
+                sw.Close();
+            }
+        }
+    }
+}
